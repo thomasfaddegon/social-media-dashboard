5a43131e1785a0a5386ffb041872214f1a4d2de0
Thomas Faddegon <tomfaddegon@gmail.com>
2023-06-08 11:45:18 -0400

    grid adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App: React.FC = () => {
       }`}
     >
       <div className="column">
-        <div className="flex w-full justify-between">
+        <div className="flex w-full justify-between items-center">
           <div className="mb-12">
             <h1 className="text-start">Social Media Dashboard</h1>
             <h3 className="text-start">Total Followers: 23,004</h3>
@@ -30,7 +30,7 @@ const App: React.FC = () => {
               checked={darkMode}
               onChange={setDarkMode}
               className={`${darkMode ? "bg-green" : "bg-[#AEB3CB]"}
-  relative inline-flex h-[38px] w-[75px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+relative inline-flex h-[38px] w-[75px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
             >
               <span className="sr-only">Use setting</span>
               {/* This is the circle */}
@@ -41,7 +41,7 @@ const App: React.FC = () => {
                     ? "translate-x-0 bg-[#333A55] "
                     : "translate-x-8 bg-[#F1F3FA]"
                 }
-    pointer-events-none inline-block h-[28px] w-[28px] transform rounded-full shadow-lg ring-0 transition duration-200 ease-in-out mt-[3px] ml-[5px]`}
+pointer-events-none inline-block h-[28px] w-[28px] transform rounded-full shadow-lg ring-0 transition duration-200 ease-in-out mt-[3px] ml-[5px]`}
               />
             </Switch>
           </div>
